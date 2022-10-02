ee4fcf507fa7f1c2062b985c2109808d223a4e38
MohammadShameem <mohammadshamimcse@gmail.com>
2022-10-02 22:39:42 +0600

    Creating a image card composable done

--- app/src/main/java/com/shameem/composefirstlesson/MainActivity.kt
+++ app/src/main/java/com/shameem/composefirstlesson/MainActivity.kt
@@ -3,17 +3,24 @@ package com.shameem.composefirstlesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.shameem.composefirstlesson.ui.theme.ComposeFirstLessonTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,11 +29,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFirstLessonTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Shameem + Shahida!")
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val painter = painterResource(id = R.drawable.ic_launcher_background)
+                    val description = "Hello World"
+                    val title ="Also Hello World"
+                    Box(modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .padding(16.dp)) {
+                        ImageCard(painter = painter
+                            , contentDescription =description,
+                            title =title)
+                    }
+                
                 }
             }
         }
@@ -34,21 +48,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()) {
-        Text(text = "Welcome to Home", textAlign = TextAlign.Center)
-        Text(text = name, textAlign = TextAlign.Center)
+fun ImageCard(
+    painter: Painter,
+    contentDescription:String,
+    title:String,
+    modifier: Modifier = Modifier
+){
+    Card(modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation()
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = 330f
+                )))
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color=Color.White, fontSize =16.sp ) )
+            }
+            
+            
+        }
+
     }
 
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column(){
+        Text(text = "Welcome to Home", textAlign =
+        TextAlign.Center,color=Color.White)
+        Text(text = name, textAlign = TextAlign.Center,
+            color= Color.White)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeFirstLessonTheme {
-        Greeting("Android")
-    }
+        Surface(modifier = Modifier.fillMaxSize()) {
+            val painter = painterResource(id = R.drawable.ic_launcher_background)
+            val description = "Hello World"
+            val title ="Also This a text for test."
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)) {
+                ImageCard(painter = painter
+                    , contentDescription =description,
+                    title =title)
+            } }
+        }
+
 }
